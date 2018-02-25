be9149206b6e682e533c235653d7b4edf9412a5b
Lihle <lihlezytumain@gmail.com>
2018-02-25 21:40:47 +0200

    Frog

--- FrogJmp/FrogJmp/Program.cs
+++ FrogJmp/FrogJmp/Program.cs
@@ -8,8 +8,25 @@ namespace FrogJmp
 {
     class Program
     {
+        static int solution(int X, int Y, int D)
+        {
+            int steps = 0;
+            for (int i=X; i <= Y; i += D)
+            {
+                steps++;
+            }
+            //while (X + D < Y)
+            //{
+            //    X += D;
+            //    steps++;
+            //}
+            return steps;
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(solution(10, 85, 30));
+            Console.ReadLine();
         }
     }
 }
